474fe3d6f26167a6112f89996e0fa942ba46518a
liaojie1314 <2020211360@stu.cqupt.edu.cn>
2022-07-13 16:18:16 +0800

    ViewBinding

--- viewmodel/src/main/java/io/liaojie1314/viewmodel/viewModelAndFragment/FragmentActivity.kt
+++ viewmodel/src/main/java/io/liaojie1314/viewmodel/viewModelAndFragment/FragmentActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import io.liaojie1314.viewmodel.R
+import io.liaojie1314.viewmodel.databinding.ActivityFragmentBinding
 
 class FragmentActivity : AppCompatActivity() {
-//    private val viewModel by lazy {
-//        ViewModelProvider(this).get(BlankViewModel::class.java)
-//    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[BlankViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+//        setContentView(R.layout.activity_fragment)
+        val viewBinding = ActivityFragmentBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        viewBinding.btnAddOne.setOnClickListener {
+            viewModel.addOne()
+        }
     }
 
 //    fun addOne(view: View) {
--- viewmodel/src/main/java/io/liaojie1314/viewmodel/viewModelAndFragment/RightFragment.kt
+++ viewmodel/src/main/java/io/liaojie1314/viewmodel/viewModelAndFragment/RightFragment.kt
@@ -9,29 +9,32 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import io.liaojie1314.viewmodel.R
+import io.liaojie1314.viewmodel.databinding.FragmentLeftBinding
 
 class RightFragment : Fragment() {
 
     private val viewModel: BlankViewModel by activityViewModels()
+    private lateinit var viewBinding:FragmentLeftBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_right, container, false)
+    ): View {
+        viewBinding =
+            FragmentLeftBinding.inflate(inflater, container, false)
+        return viewBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val tvRight = view.findViewById<TextView>(R.id.tv_right)
-        view.findViewById<Button>(R.id.btn_right).apply {
+        viewBinding.btnLeft.apply {
             setOnClickListener {
                 viewModel.addOne()
             }
         }
         activity?.let { it ->
             viewModel.getLiveData().observe(it) {
-                tvRight.text = it.toString()
+                viewBinding.tvLeft.text = it.toString()
             }
         }
     }
